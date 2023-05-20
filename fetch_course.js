655a8132f95b30468147f4d169483556a2694b79
Andrew Ursu <aursu@sfu.ca>
2023-05-20 16:59:27 -0700

    got all request functions up

--- fetch_course.js
+++ fetch_course.js
@@ -2,7 +2,8 @@
 function findCourse(year, semester, department, number) {
   const axios = require('axios');
 
-  axios.get('http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}')
+  axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}`)
+  // axios.get('http://www.sfu.ca/bin/wcm/course-outlines?2022/Fall/CMPT/379')
     .then(function (response) {
       console.log(response.data);
     })
@@ -12,4 +13,52 @@ function findCourse(year, semester, department, number) {
 
 }
 
-findCourse("2022", "Fall", "CMPT", "379")
+function findByDepartment(year, semester, department) {
+  const axios = require('axios');
+
+  axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}`)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log('Error:', error.message);
+    });
+
+}
+
+function findBySemester(year, semester) {
+  const axios = require('axios');
+
+  axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}`)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log('Error:', error.message);
+    });
+
+}
+
+function findByYear(year) {
+  const axios = require('axios');
+
+  axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}`)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log('Error:', error.message);
+    });
+
+}
+// console.log("Find by Year")
+// findByYear(2022)
+// console.log()
+// console.log("Find by Semester")
+// findBySemester(2022, 'Fall')
+// console.log()
+// console.log("Find by Department")
+// findByDepartment(2022, 'Fall', 'CMPT')
+// console.log()
+// console.log("Find Course")
+// findCourse(2022, 'Fall', 'CMPT', 379)
