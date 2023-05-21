53fefe60af31dbe9d9008fae9aa91929a7ddc402
icextreme <74882450+icextreme@users.noreply.github.com>
2023-05-20 19:31:42 -0700

    Fix csss command name

--- commands/csss.js
+++ commands/csss.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
+		.setName('csss')
 		.setDescription('John Wu\'s favourite quote'),
 	async execute(interaction) {
 		await interaction.reply('CSSS is love, CSSS is life.');
