3d002ed411894a7e83aba16bf8f4c77dc8695c2a
icextreme <74882450+icextreme@users.noreply.github.com>
2023-05-21 11:04:16 -0700

    Fix formatting

--- commands/register.js
+++ commands/register.js
@@ -1,40 +1,50 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("register")
-    .setDescription("add yourself to a class")
-    .addStringOption((option) =>
-      option
-        .setName("class_name")
-        .setDescription("The name of the class")
-        .setRequired(true)
-    ),
+    data: new SlashCommandBuilder()
+        .setName("register")
+        .setDescription("add yourself to a class")
+        .addStringOption((option) =>
+            option
+                .setName("class_name")
+                .setDescription("The name of the class")
+                .setRequired(true)
+        ),
 
-  async execute(interaction) {
-    const roleName = interaction.options.getString("class_name");
-    console.log(roleName);
+    async execute(interaction) {
+        const roleName = interaction.options.getString("class_name");
+        console.log(roleName);
 
-    let role = interaction.member.guild.roles.cache.find(x => x.name == roleName);
-    // Create the role in the server
-    if (role) {
-      interaction.member.roles.add(role);
-      interaction.reply(`Role "${role.name}" assigned!`).catch((error) => {
-        console.error("Error adding role:", error);
-        interaction.reply("An error occurred while adding the role.");
-      });
-    } else {
-      interaction.guild.roles
-        .create({ name: roleName })
-        .then((role) => {
-          // Assign the created role to the interaction user
-          interaction.member.roles.add(role);
-          interaction.reply(`Role "${role.name}" created and assigned!`);
-        })
-        .catch((error) => {
-          console.error("Error creating role:", error);
-          interaction.reply("An error occurred while creating the role.");
-        });
-    }
-  },
+        let role = interaction.member.guild.roles.cache.find(
+            (x) => x.name == roleName
+        );
+        // Create the role in the server
+        if (role) {
+            interaction.member.roles.add(role);
+            interaction
+                .reply(`Role "${role.name}" assigned!`)
+                .catch((error) => {
+                    console.error("Error adding role:", error);
+                    interaction.reply(
+                        "An error occurred while adding the role."
+                    );
+                });
+        } else {
+            interaction.guild.roles
+                .create({ name: roleName })
+                .then((role) => {
+                    // Assign the created role to the interaction user
+                    interaction.member.roles.add(role);
+                    interaction.reply(
+                        `Role "${role.name}" created and assigned!`
+                    );
+                })
+                .catch((error) => {
+                    console.error("Error creating role:", error);
+                    interaction.reply(
+                        "An error occurred while creating the role."
+                    );
+                });
+        }
+    },
 };
