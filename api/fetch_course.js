6ebef46e8ee451b4c2c5a81febb6c65e822d3b0b
icextreme <74882450+icextreme@users.noreply.github.com>
2023-05-21 11:13:18 -0700

    Update axios to get https url and fix course command description

--- api/fetch_course.js
+++ api/fetch_course.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 
 async function findSection(year, semester, department, number, section) {
-  return await axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}/${section}`)
+  return await axios.get(`https://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}/${section}`)
 }
 
 function findCourseWithDescription(year, semester, department, number) {
 
-  axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}/D100`)
+  axios.get(`https://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}/D100`)
     .then(function (response) {
       console.log(response.data);
     })
@@ -17,11 +17,11 @@ function findCourseWithDescription(year, semester, department, number) {
 }
 
 async function findCourse(year, semester, department, number) {
-  return await axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}`)
+  return await axios.get(`https://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}/${number}`)
 }
 
 async function findCourses(year, semester, department) {
-  return await axios.get(`http://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}`)
+  return await axios.get(`https://www.sfu.ca/bin/wcm/course-outlines?${year}/${semester}/${department}`)
 }
 
 
